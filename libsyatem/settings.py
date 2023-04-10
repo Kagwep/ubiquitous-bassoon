3a13d603208de909c692928c63a3e9bddba5c87b
kagwe <kagwepeter07@gmail.com>
2023-04-10 17:32:26 +0300

    sc

--- libsyatem/settings.py
+++ libsyatem/settings.py
@@ -90,13 +90,24 @@ WSGI_APPLICATION = 'libsyatem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'HOST': "db.vqwhgtyleptkzlamdelw.supabase.co",
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "@Karis2020!",
+        'PORT': "5432",
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
